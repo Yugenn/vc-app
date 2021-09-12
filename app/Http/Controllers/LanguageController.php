25843fb56224827b19032d34e73d18998a74a8f7
Yugenn <yuruseajyan@gmail.com>
2021-09-12 13:52:51 +0900

    Language実装

---
+++ app/Http/Controllers/LanguageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LanguageController extends Controller
+
+
+{
+    public function index ($language){
+
+    $language = ['HTML', 'CSS', 'JavaScript'];
+    return view('language.index', ['language' => $language]);
+}
+
+    
+}
