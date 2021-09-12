4b2222556cdf1b936a86e9a82c89c83f70dd6e4f
Yugenn <yuruseajyan@gmail.com>
2021-09-12 13:06:52 +0900

    VAApp完成！！

--- resources/views/message/word.blade.php
+++ resources/views/message/word.blade.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>VCApp</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
     <h1>URLパラメータを表示</h1>
-    <h2>{{ $msg }} </h2>
+    <h2>{{ $message }} </h2>
     
 </body>
 </html>
